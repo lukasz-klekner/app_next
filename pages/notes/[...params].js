5b00ed4c0d0b16fe2a0871739dbc85422af29021
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-31 11:36:34 +0100

    adds routing

---
+++ pages/notes/[...params].js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+
+const Page = () => {
+  const router = useRouter()
+  const { params } = router.query
+
+  console.log(params)
+
+  return <div>Note </div>
+}
+
+export default Page
---
+++ pages/notes/index.js
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Note Page Index</div>
+}
+
+export default Page
